c85bd56ae89c16830519641ab4cf064e6cdd5cc6
larkinds <larkindsx@gmail.com>
2020-04-01 19:09:04 +0700

    NewsService interface for crud operations added.

--- src/main/java/com/larkinds/sibers/service/NewsService.java
+++ src/main/java/com/larkinds/sibers/service/NewsService.java
@@ -1,5 +1,34 @@
 package com.larkinds.sibers.service;
 
-public interface NewsService {
+import com.larkinds.sibers.dto.NewsDto;
 
+import java.util.List;
+
+public interface NewsService {
+    /**
+     * Adds news to the database
+     * @param newsDto DTO representation of the news entity
+     * @return news added
+     */
+    NewsDto add(NewsDto newsDto);
+
+    /**
+     * Returns all news stored in the database
+     * @return List of news stored in the database
+     */
+    List<NewsDto> getAll();
+
+    /**
+     * Updates the news by its id
+     * @param id id of the news
+     * @return updated news dto
+     */
+    NewsDto update(Long id);
+
+    /**
+     * Returns a limited count of news
+     * @param limit limiter
+     * @return a list of news matching param criteria
+     */
+    List<NewsDto> getAllWithPagination(Integer limit);
 }
