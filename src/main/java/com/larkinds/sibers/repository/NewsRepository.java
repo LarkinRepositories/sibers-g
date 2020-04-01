d300d1968c18c5a01ddd6d717e898b839bdfc09b
larkinds <larkindsx@gmail.com>
2020-04-01 18:41:29 +0700

    News repository implemented

---
+++ src/main/java/com/larkinds/sibers/repository/NewsRepository.java
@@ -0,0 +1,9 @@
+package com.larkinds.sibers.repository;
+
+import com.larkinds.sibers.model.News;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NewsRepository extends JpaRepository<News, Long> {
+}
