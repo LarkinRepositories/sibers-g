e9e452e6f33d260f6e89396e6f749c67e51dc07d
larkinds <larkindsx@gmail.com>
2020-04-01 19:44:05 +0700

    Commentary section added

--- src/main/java/com/larkinds/sibers/dto/NewsDto.java
+++ src/main/java/com/larkinds/sibers/dto/NewsDto.java
@@ -3,6 +3,11 @@ package com.larkinds.sibers.dto;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+
+/**
+ * Data to object class describing News class
+ */
+
 @Data
 public class NewsDto {
     private final Long id;
