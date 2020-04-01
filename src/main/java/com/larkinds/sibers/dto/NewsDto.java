cf87a18933f05842f5a473dde522ed6ff9eebe94
larkinds <larkindsx@gmail.com>
2020-04-01 19:00:41 +0700

    DTO NewsDto implemented

---
+++ src/main/java/com/larkinds/sibers/dto/NewsDto.java
@@ -0,0 +1,13 @@
+package com.larkinds.sibers.dto;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+@Data
+public class NewsDto {
+    private final Long id;
+    private final LocalDateTime created;
+    private String img;
+    private final String  message;
+}
