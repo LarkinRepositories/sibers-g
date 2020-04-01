5858e1c36498601498b2d831e4b97fc589ed56de
larkinds <larkindsx@gmail.com>
2020-04-01 18:37:19 +0700

    Model class News.java describing news entity implemented

---
+++ src/main/java/com/larkinds/sibers/model/News.java
@@ -0,0 +1,23 @@
+package com.larkinds.sibers.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "news")
+public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @CreatedDate
+    @Column(name = "created")
+    private LocalDateTime created;
+    @Column(name = "img")
+    private String img;
+    @Column(name = "message")
+    private String  message;
+}
