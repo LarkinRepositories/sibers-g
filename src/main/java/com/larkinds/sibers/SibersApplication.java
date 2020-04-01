cbb26badefe08ae34fb802ff539d623e45095cf4
larkinds <larkindsx@gmail.com>
2020-04-01 18:11:52 +0700

    Initial commit

---
+++ src/main/java/com/larkinds/sibers/SibersApplication.java
@@ -0,0 +1,13 @@
+package com.larkinds.sibers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SibersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SibersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/larkinds/sibers/SibersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larkinds.sibers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SibersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
