3dbc41a319798a22a761c6814c4e4d66f10d0752
larkinds <larkindsx@gmail.com>
2020-04-03 13:14:27 +0700

    ready to be deployed on tomcat

--- src/main/java/com/larkinds/sibers/SibersApplication.java
+++ src/main/java/com/larkinds/sibers/SibersApplication.java
@@ -2,9 +2,16 @@ package com.larkinds.sibers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SibersApplication {
+public class SibersApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SibersApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SibersApplication.class, args);
